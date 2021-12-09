c80b4101f6f5e24ad8686d2443a91e7051d4ec62
George Kaye <georgejkaye@gmail.com>
2021-12-09 18:03:53 +0000

    Created generator of state values

--- src/Mealy.ml
+++ src/Mealy.ml
@@ -1,3 +1,11 @@
+let rec check_for_dups = function
+| [] -> false
+| (x :: xs) -> if List.mem x xs then true else check_for_dups xs
+
+let rec remove_dups = function
+| [] -> []
+| (x :: xs) -> if List.mem x xs then remove_dups xs else x :: remove_dups xs
+
 type value = Bottom | True | False | Top
 
 let vtos = function
@@ -30,11 +38,13 @@ let rec concat_no_dups ys = function
 | [] -> ys
 | (x :: xs) -> if (List.mem x ys) then (concat_no_dups ys xs) else (x :: concat_no_dups xs ys)
 
-let rec possible_inputs = function
-    | 0 -> [[]]
-    | n -> let recurse = possible_inputs (n-1) in 
-           let current = List.map (fun x -> all_insertions x) recurse in
-           List.fold_left (fun acc -> fun cur -> concat_no_dups acc cur) [] current
+let possible_inputs i =
+    let rec possible_inputs' = function
+        | 0 -> [[]]
+        | n -> let recurse = possible_inputs' (n-1) in 
+            let current = List.map (fun x -> all_insertions x) recurse in
+            List.fold_left (fun acc -> fun cur -> concat_no_dups acc cur) [] current in
+    List.rev (possible_inputs' i)
 
 let extend_word w i = 
     let inputs = possible_inputs i in
@@ -58,6 +68,11 @@ type mealy = {
     functions: (int -> (value list -> int * value list));
 }
 
+type path = {
+    inputs: value list list;
+    states: int list;
+    outputs: value list list;
+}
 
 type trace = int list * value list list
 
@@ -67,7 +82,10 @@ let compute_trace mm ins =
                         ( next , next :: path, inp :: ins, outs :: word )
     ) (0, [0], [], []) ins
     in
-    (List.rev ins, List.rev path, List.rev word)
+    {inputs = List.rev ins; states = List.rev path; outputs = List.rev word}
+
+let next_states mm s = 
+    List.map (fun v -> let (next, _) = mm.functions s v in next) (possible_inputs mm.input)
 
 let rec print_list del print = function
 | [] -> ""
@@ -78,16 +96,16 @@ let print_trace t = print_list " " string_of_int t
 
 let print_input i = "[" ^ print_list " " vtos i ^ "]"
 let print_word w = print_list " " print_input w
-let print_path = function
-| (inword, trace, outword) -> ( print_word inword, print_trace trace, print_word outword)
+let print_path p = ( print_word p.inputs, print_trace p.states, print_word p.outputs )
+
 
 let paths mm i = 
     let ws = possible_words mm.input i in
     List.fold_left (
         fun acc -> 
             fun cur -> 
-                let (states, ins, words) = compute_trace mm cur in
-                (states, ins, words) :: acc                
+                let trace = compute_trace mm cur in
+                trace :: acc                
      ) [] ws 
 
 let all_paths mm = 
@@ -104,8 +122,7 @@ let all_paths mm =
         ) inputs) in
         List.fold_left (fun acc -> fun cur -> concat_no_dups acc cur) [] nexts
 in let paths = all_paths' [] 0 [] [] []
-in List.map ( fun (inp, trace, oup) -> (List.rev inp), (List.rev trace), (List.rev oup)) paths
-
+in List.map ( fun (inp, trace, oup) -> {inputs = inp; states = trace; outputs = oup }) paths
 
 let fs0 = function
 | [Bottom] -> (1 , [True; Bottom])
@@ -135,21 +152,78 @@ let example = {
     input = 1;
     output = 2;
     states = 4;
-    functions = (fun s -> match s with | 0 -> fs0 | 1 -> fs1 | 2 -> fs2 | 3 -> fs3 | n -> failwith "type error")
+    functions = (fun s -> match s with | 0 -> fs0 | 1 -> fs1 | 2 -> fs2 | 3 -> fs3 | n -> failwith "type error");
 }
-(* 
-(* let find_paths mm = 
-    let rec find_paths' mm cur explored = 
-    List.map (fun )  *)
 
-in find_paths' mm 0 [] *)
+(* Assign values to each states 
 
-let rec check_for_dups = function
-| [] -> false
-| (x :: xs) -> if List.mem x xs then true else check_for_dups xs
+   We can't use 'pure' one-hot encodings since we need to preserve monotonicity. 
+   However (thanks to David Sprunger) there is a way, as long as V is a power of 2.
 
+   Say we have states {s0, s1, s2, s3} and the order
+
+                s3
+              /   \
+            s1    s2
+              \  /
+               s0
+
+   Then we can encode these in a way compatible with monotonicity by assigning them
+   each a bitstring of 4 bits, where b_k is 1 if s >= s_k and 0 otherwise.
+*)
+
+(* let compute_order mm = 
+    let ps = all_paths mm in
+ *)
+
+
+let lte x y = match (x, y) with
+| (_, Top) -> Some true
+| (Top, _) -> Some false
+| (Bottom, _) -> Some true
+| (_, Bottom) -> Some false
+| (_, _) -> None
+
+let path_lte xs ys = if List.length xs != List.length ys then None else 
+    let bits = List.map2 lte xs ys in
+    if List.mem None bits then None else if List.mem (Some true) bits && List.mem (Some false) bits then None else List.hd bits
+
+
+
+(* Printers *)
 
 open Format
+let print_all_transitions mm =
+    let print_transitions_from_state i = 
+        let print_transition x = 
+            let (state, output) = mm.functions i x in
+            print_string ("s" ^ (string_of_int i) ^ " --- " ^ (print_input x) ^ " | " ^ (print_input output) ^ " ---> s" ^ (string_of_int state) ^ "\n")
+        in
+        List.fold_left (fun acc -> fun cur -> print_transition cur) () (possible_inputs mm.input)
+    in
+    List.fold_left (fun acc -> fun cur -> print_transitions_from_state cur) () (List.init mm.states (fun i -> i))
+
+let mealy_printer mm = 
+    print_string ((string_of_int mm.input) ^ "--" ^ (string_of_int mm.states) ^ "-->" ^ (string_of_int mm.output)) ; print_string "\n" ; print_all_transitions mm
+    
+
 let rec path_list_printer = function
 | [] -> ()
 | (x :: xs) -> let (a,b,c) = print_path x in print_string a ; print_string "  |  " ; print_string b ; print_string "  |  " ; print_string c ; print_string "\n"; path_list_printer xs
+
+
+type partial_order = int -> int -> bool option
+
+let expo i j = match (i, j) with
+| (0, _) -> None
+| (_, 0) -> None
+| (1, _) -> Some true
+| (_, 1) -> Some false
+| (2, _) -> Some true
+| (_, 2) -> Some false
+| (3, 3) -> Some true
+| _ -> failwith "not enough states"
+
+let generate_state_values i po = List.map (
+    fun s -> List.init i (fun i -> match po i s with | Some b -> if b then True else False | None -> False)
+) (List.init i (fun x -> x))
